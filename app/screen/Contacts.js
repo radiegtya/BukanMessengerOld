ef5d893d42d943eb168be5e6842871d866337a35
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-07-20 15:38:25 +0700

    contacts 50 percent done

--- app/screen/Contacts.js
+++ app/screen/Contacts.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
 import {} from 'react-native';
 import {Container, Content, Header, Left, Body, Right, Text, Title, ListItem, List, Thumbnail, Item, Input, Icon} from 'native-base';
+import RNContacts from 'react-native-contacts';
+import Meteor, {connectMeteor} from 'react-native-meteor';
+import {MO} from '../MO';
 
+@connectMeteor
 export default class Contacts extends Component {
 
   static navigationOptions = {
@@ -9,7 +13,40 @@ export default class Contacts extends Component {
     tabBarIcon: ({ tintColor }) => (
       <Icon name="person" style={{color:tintColor}}/>
     ),
-  };
+  }
+
+  constructor(){
+    super();
+  }
+
+  getMeteorData(){
+    const self = this;
+    console.log(MO.user().username)
+    RNContacts.getAll((err, contacts) => {
+      if(err === 'denied'){
+        // x.x
+      } else {
+        let phoneNumbers = [];
+        contacts.forEach((contact)=>{
+          contact.phoneNumbers.forEach((phone)=>{
+            if(phone.number){
+              const formatedPhoneNumber = "+62" + phone.number.replace(new RegExp(/[-\/\\^$*+?.()|[\]{}]/g, 'g'), '').replace(/\s/g,'');
+              phoneNumbers.push(formatedPhoneNumber);
+            }
+          });
+        });
+        phoneNumbers = [...new Set(phoneNumbers)];
+
+        //first param is unique subName (client), second is real subscription name, third is query, then fourth is callback
+        MO.subscribe('contactsSub', 'users', {username: {$in: phoneNumbers}}, ()=>{});
+      }
+    });
+
+    return {
+      //first param is collectionName, second is unique subscription name (client)
+      contacts: MO.collection('users', 'contactsSub').find({username: {$ne: MO.user().username}}),
+    }
+  }
 
   _renderHeader(){
     return (
@@ -23,14 +60,14 @@ export default class Contacts extends Component {
     );
   }
 
-  _renderRow(){
+  _renderRow(contact, i){
     return (
-      <ListItem avatar>
+      <ListItem avatar key={i}>
         <Left>
           <Thumbnail small source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }} />
         </Left>
         <Body>
-          <Text>Kumar Pratik</Text>
+          <Text>{contact.username}</Text>
         </Body>
         <Right/>
       </ListItem>
@@ -54,7 +91,7 @@ export default class Contacts extends Component {
 
           {/* List */}
           <List>
-            {this._renderRow()}
+            {this.data.contacts.map((contact,i) => this._renderRow(contact, i))}
           </List>
           {/* List End */}
 
@@ -66,6 +103,7 @@ export default class Contacts extends Component {
   }
 
 }
+
 
 //NativeBase styling basic obj
 const styles = {
