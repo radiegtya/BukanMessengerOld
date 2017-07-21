bc2949ab5b25270128216573f76207814533f237
Ega Radiegtya <radiegtya@Egas-MacBook-Pro.local>
2017-07-21 08:16:19 +0700

    using state instead navigation params on newContact

--- app/screen/NewContact.js
+++ app/screen/NewContact.js
@@ -1,37 +1,41 @@
 import React, {Component} from 'react';
 import {TouchableOpacity} from 'react-native';
-import { Container, Content, Form, Item, Input, Label, Button, Text } from 'native-base';
+import { Container, Content, Form, Item, Input, Label, Button, Text, Header, Left, Body, Right } from 'native-base';
 import RNContacts from 'react-native-contacts';
 
 export default class NewContact extends Component{
 
-  static navigationOptions = ({navigation}) => {
-    const {params = {}} = navigation.state;
-    let validationCondition = params.firstName != "" && params.mobileNumber != "";
-    return {
-      title: 'New Contact',
-      headerRight: (
-        <TouchableOpacity onPress={()=>params.handleDone(validationCondition)}>
-          <Text style={{color: validationCondition ?'#4285f4':'#d0d0d0', marginRight: 10}}>Done</Text>
-        </TouchableOpacity>
-      )
-    }
-  };
-
-  componentWillMount(){
-    //params for navigation
-    this.props.navigation.setParams({
+  constructor(){
+    super();
+    this.state = {
       firstName: "",
       lastName: "",
       mobileNumber: "",
-      handleDone: this.handleDone.bind(this)
-    });
+    };
+  }
+
+  _renderHeader(){
+    const {firstName, lastName, mobileNumber} = this.state;
+    const validationCondition = firstName != "" && mobileNumber != "";
+    return (
+      <Header>
+        <Left/>
+        <Body>
+          <Text>Contacts</Text>
+        </Body>
+        <Right>
+        <TouchableOpacity onPress={()=>this.handleDone(validationCondition)}>
+          <Text style={{color: validationCondition ?'#4285f4':'#d0d0d0', marginRight: 10}}>Done</Text>
+        </TouchableOpacity>
+        </Right>
+      </Header>
+    )
   }
 
   handleDone(validationCondition){
+    const {firstName, lastName, mobileNumber} = this.state;
     if(validationCondition){
         const {goBack} = this.props.navigation;
-        const {firstName, lastName, mobileNumber} = this.props.navigation.state.params;
         RNContacts.addContact({
           givenName: firstName + " " + lastName,
           phoneNumbers: [
@@ -52,15 +56,17 @@ export default class NewContact extends Component{
     return (
       <Container>
         <Content>
+          {this._renderHeader()}
+
           <Form>
             <Item>
-              <Input placeholder="First Name" onChangeText={(text) => this.props.navigation.setParams({firstName: text})}/>
+              <Input placeholder="First Name" onChangeText={(text) => this.setState({firstName: text})}/>
             </Item>
             <Item inlineLabel>
-              <Input placeholder="Last Name" onChangeText={(text) => this.props.navigation.setParams({lastName: text})}/>
+              <Input placeholder="Last Name" onChangeText={(text) => this.setState({lastName: text})}/>
             </Item>
             <Item inlineLabel>
-              <Input placeholder="Mobile Number" onChangeText={(text) => this.props.navigation.setParams({mobileNumber: text})}/>
+              <Input placeholder="Mobile Number" onChangeText={(text) => this.setState({mobileNumber: text})}/>
             </Item>
           </Form>
         </Content>
